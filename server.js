3b53c73e4a7ade8c2ec2da90738bc6c9980b09c2
susukacang <yongsoonfatt@gmail.com>
2023-05-28 16:03:52 -0700

    add about route

--- server.js
+++ server.js
@@ -5,35 +5,38 @@ console.log("hello basics.....");
 
 // ES6 (include "type":"module" inside package.json)
 import express from "express";
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const x = 1
+const x = 1;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.listen(5002, () => {
-    console.log("Basics server listening on port 5002");
-  });
-
-
+  console.log("Basics server listening on port 5002");
+});
 
 app.get("/", (req, res) => {
-  res.json({"msg":"hello basics"});
+  res.json({ msg: "hello basics" });
 });
 
 app.get("/demo", (req, res) => {
-  console.log(process.env)
-    res.json({"msg":"hello demo", "secret":process.env.REACT_APP_SECRET_KEY});
-  });
+  console.log(process.env);
+  res.json({ msg: "hello demo", secret: process.env.REACT_APP_SECRET_KEY });
+});
 
-app.set('view engine', "ejs")
+app.set("view engine", "ejs");
 
 app.get("/home", (req, res) => {
-    res.render("home", {"msg":"hello one", "secret":"123456"});
-  }); 
+  res.render("home", { msg: "hello one", secret: "123456" });
+});
 
+app.get("/about", (req, res) => {
+  res.json({ msg: "about basics" });
+});
